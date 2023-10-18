45af487b2102de89118624655e7f5d4a9da8ac67
Stefan Freitag <stefan.freitag@udo.edu>
2023-10-18 20:56:48 +0200

    feat: add support for suppressing notifications (#3)
    Users can specify MSK cluster states that should not trigger a notification.
    - Updated pre-commit hooks to use newer versions:
      - antonbabenko/pre-commit-terraform@v1.83.5
      - pre-commit/pre-commit-hooks@v4.5.0

--- data.tf
+++ data.tf
@@ -8,5 +8,5 @@ data "aws_region" "current" {}
 data "archive_file" "status_checker_code" {
   type        = "zip"
   source_dir  = "${path.module}/functions/check-msk-status/"
-  output_path = "${path.module}/python/hello-python.zip"
+  output_path = "${path.module}/out/check-msk-status.zip"
 }
--- functions/check-msk-status/index.py
+++ functions/check-msk-status/index.py
@@ -1,37 +1,47 @@
 import boto3
 import os
 
+
 def lambda_handler(event, context):
-    LAMBDASNSTOPIC = os.environ['SNS_TOPIC_ARN']
-    region = 'eu-central-1'
+    LAMBDASNSTOPIC = os.environ["SNS_TOPIC_ARN"]
+    SUPPRESS_STATES = os.environ["SUPPRESS_STATES"].split(",")
+    region = "eu-central-1"
     # Create an MSK client
-    client = boto3.client('kafka', region_name=region)
+    client = boto3.client("kafka", region_name=region)
     # Retrieve a list of clusters
     response = client.list_clusters()
     # Extract the cluster ARNs from the response
-    cluster_arns = response['ClusterInfoList']
+    cluster_arns = response["ClusterInfoList"]
+
+    valid_states = ["ACTIVE"] + SUPPRESS_STATES
+    print(
+        "Notifications suppressed for these MSK states: {}".format(
+            ", ".join(valid_states)
+        )
+    )
 
     for cluster in cluster_arns:
-        arn = cluster['ClusterArn']
+        arn = cluster["ClusterArn"]
         response = client.describe_cluster(ClusterArn=arn)
-        status = response['ClusterInfo']['State']
-        sns_client = boto3.client('sns')
-
-        if status != 'ACTIVE':
-          print("The MSK cluster: {} needs attention.".format(arn))
-          sns_client.publish(TopicArn=LAMBDASNSTOPIC,
-                           Message="MSK cluster: " + arn + " needs attention. The status is: " + status,
-                           Subject="MSK Health Warning!")
+        status = response["ClusterInfo"]["State"]
+        print("The cluster is in state {}.".format(status))
+        sns_client = boto3.client("sns")
+        if status not in valid_states:
+            print("The MSK cluster: {} needs attention.".format(arn))
+            sns_client.publish(
+                TopicArn=LAMBDASNSTOPIC,
+                Message="MSK cluster: "
+                + arn
+                + " needs attention. The status is: "
+                + status,
+                Subject="MSK Health Warning!",
+            )
         else:
-          print(
-            "The MSK cluster: {} is in a healthy state, and is reachable and available for use.".format(
-                arn))
+            print(
+                "The MSK cluster: {} is in a healthy state, and is reachable and available for use.".format(
+                    arn
+                )
+            )
 
     # Return the status
-    return {
-        'statusCode': 200,
-        'body': 'OK'
-    }
-
-if __name__ == '__main__':
-    lambda_handler(None, None)
+    return {"statusCode": 200, "body": "OK"}
