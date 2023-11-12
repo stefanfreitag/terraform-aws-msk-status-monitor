ba265a41230ecc98f097f219751e3bf0a92108fb
Stefan Freitag <stefan.freitag@udo.edu>
2023-11-12 06:34:50 +0100

    fix: set output_file_mode for zip creation

--- data.tf
+++ data.tf
@@ -4,9 +4,11 @@ data "aws_caller_identity" "current" {}
 # AWS region information
 data "aws_region" "current" {}
 
-
+# Creates a zip file for the check-msk-status Lambda function
 data "archive_file" "status_checker_code" {
-  type        = "zip"
-  source_dir  = "${path.module}/functions/check-msk-status/"
-  output_path = "${path.module}/out/check-msk-status.zip"
+  type                        = "zip"
+  source_dir                  = "${path.module}/functions/check-msk-status/"
+  output_path                 = "${path.module}/out/check-msk-status.zip"
+  exclude_symlink_directories = false
+  output_file_mode            = "0666"
 }
